aa54eb07f442750d88586a892d559080bbb7bb82
Carson Keller <carsonkeller@u.boisestate.edu>
2023-02-24 20:08:21 -0700

    Completed functionality and javadocs

--- TaskGenerator.java
+++ TaskGenerator.java
@@ -1,17 +1,46 @@
+import java.util.Random;
+
 /**
- *
+ * This class details the construction and use of TaskGenerators to be created
+ * and used in the public MyLifeInStardew class as part of the game simulation.
+ * This class acts as a bridge between the Task class and the MyLifeInStardew
+ * class to determine whether actions happen based on probability and create
+ * tasks when needed.
  *
  * @author carsonkeller
  */
 public class TaskGenerator implements TaskGeneratorInterface {
     private int currentEnergyStorage;
+    private double probability;
+    private Random random;
 
-    public TaskGenerator() {
-
+    /**
+     * This is the primary constructor for the class that creates TaskGenerator
+     * objects given a provided probability value.
+     *
+     * @param probability - double value representing the probability assigned to
+     *                    this TaskGenerator
+     */
+    public TaskGenerator(double probability) {
+        currentEnergyStorage = TaskGeneratorInterface.DEFAULT_ENERGY;
+        this.probability = probability;
+        random = new Random();
     }
 
-    public TaskGenerator() {
-
+    /**
+     * This is the overloaded, secondary constructor for the class that creates
+     * TaskGenerator but requires a seed for the random generator in addition to
+     * the probability value.
+     *
+     * @param probability - double value representing the probability assigned to
+     *                    this TaskGenerator
+     * @param seed - long value representing the seed to initialize the Random
+     *             object
+     */
+    public TaskGenerator(double probability, long seed) {
+        currentEnergyStorage = TaskGeneratorInterface.DEFAULT_ENERGY;
+        this.probability = probability;
+        random = new Random(seed);
     }
 
     @Override
@@ -21,12 +50,12 @@ public class TaskGenerator implements TaskGeneratorInterface {
 
     @Override
     public void decrementEnergyStorage(Task.TaskType taskType) {
-
+        currentEnergyStorage = currentEnergyStorage - taskType.getEnergyPerHour();
     }
 
     @Override
     public void resetCurrentEnergyStorage() {
-
+        currentEnergyStorage = TaskGeneratorInterface.DEFAULT_ENERGY;
     }
 
     @Override
@@ -41,12 +70,29 @@ public class TaskGenerator implements TaskGeneratorInterface {
 
     @Override
     public boolean generateTask() {
-        return false;
+        double randProbability = random.nextDouble();
+
+        if (randProbability <= this.probability) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int getUnlucky(Task task, double unluckyProbability) {
-        return 0;
+        if (unluckyProbability <= task.getTaskType().getPassingOutProbability()) {
+            if (unluckyProbability <= task.getTaskType().getDyingProbability() && task.getTaskType() == TaskInterface.TaskType.MINING) {
+                currentEnergyStorage = currentEnergyStorage / 4;
+                task.setPriority(0);
+                return 2;
+            } else {
+                currentEnergyStorage = currentEnergyStorage / 2;
+                return 1;
+            }
+        } else {
+            return 0;
+        }
     }
 
     @Override
